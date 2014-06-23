0bbad25470f5c1eca8fd701ac0322f607627ca3c
Chris Huang <chris820702@gmail.com>
2014-06-24 01:28:53 +0800

    Update TocHw4.py

--- TocHw4.py
+++ TocHw4.py
@@ -13,47 +13,47 @@ roadName = ""
 argument = u"土地區段位置或建物區門牌"
 
 if len(sys.argv) != 2:
-    print "Error in argument."
-    sys.exit(0)
-    
+	print "Error in argument."
+	sys.exit(0)
+
 response = urllib2.urlopen(sys.argv[1])
 text = json.load(response)
 
 #---------------------------------------------------
 for i in range(len(text)):
-    roadName = ""
-    split = ""
-    if text[i][argument].find(u"大道") != -1:
-        split = text[i][argument].find(u"大道")
-        if split != 0:
-            roadName = text[i][argument][0:split+1]
-
-    elif text[i][argument].find(u"路") != -1:
-        split = text[i][argument].find(u"路")
-        if text[i][argument].find(u"路") == 1:
-        	if split != 0:
-            	roadName = text[i][argument][0:split+1]
-        else:
-        	index = text[i][argument][split+2:len(text[i][argument])].find(u"路")
-        	index2 = split + 2
-        	split = index + index2
-        	roadName = text[i][argument][0:split+1]
-
-    elif text[i][argument].find(u"街") != -1:
-        split = text[i][argument].find(u"街")
-        if split != 0:
-            roadName = text[i][argument][0:split+1]
-
-    elif text[i][argument].find(u"巷") != -1:
-        split = text[i][argument].find(u"巷")
-        if split != 0:
-            roadName = text[i][argument][0:split+1]
-    else:
-        roadName = ""
-
-    if roadName != "":
-        if road.count(roadName) == 0:
-            road.append(roadName)
+	roadName = ""
+	split = ""
+	if text[i][argument].find(u"大道") != -1:
+		split = text[i][argument].find(u"大道")
+		if split != 0:
+			roadName = text[i][argument][0:split+1]
+	
+	elif text[i][argument].find(u"路") != -1:
+		split = text[i][argument].find(u"路")
+			if text[i][argument].find(u"路") == 1:
+				if split != 0:
+					roadName = text[i][argument][0:split+1]
+			else:
+				index = text[i][argument][split+2:len(text[i][argument])].find(u"路")
+				index2 = split + 2
+				split = index + index2
+				roadName = text[i][argument][0:split+1]
+	
+	elif text[i][argument].find(u"街") != -1:
+		split = text[i][argument].find(u"街")
+		if split != 0:
+			roadName = text[i][argument][0:split+1]
+	
+	elif text[i][argument].find(u"巷") != -1:
+		split = text[i][argument].find(u"巷")
+		if split != 0:
+			roadName = text[i][argument][0:split+1]
+	else:
+		roadName = ""
+	
+	if roadName != "":
+		if road.count(roadName) == 0:
+			road.append(roadName)
 #---------------------------------------------------
 
 argumentYear = u"交易年月"
@@ -61,31 +61,32 @@ finalRoad = list()
 yearcount = 0
 
 for i in range(0,len(road)):
-    for j in range(len(text)):
-        if text[j][argument].find(road[i]) != -1:
-            if year.count(text[j][argumentYear]) == 0:
-                year.append(text[j][argumentYear])
-
-    if len(year) > yearcount:
-        del finalRoad[0:len(finalRoad)]
-        finalRoad.append(road[i])
-        yearcount = len(year)
-    elif len(year) == yearcount:
-        finalRoad.append(road[i])
-
-    del year[0:len(year)]
+	for j in range(len(text)):
+		if text[j][argument].find(road[i]) != -1:
+			if year.count(text[j][argumentYear]) == 0:
+				year.append(text[j][argumentYear])
+		
+	if len(year) > yearcount:
+		del finalRoad[0:len(finalRoad)]
+		finalRoad.append(road[i])
+		yearcount = len(year)
+	elif len(year) == yearcount:
+		finalRoad.append(road[i])
+		
+	del year[0:len(year)]
 
 #---------------------------------------------------
 
 price = list()
 
 for i in range(0, len(finalRoad)):
-    for j in range(0, len(text)):
-        if text[j][argument].find(finalRoad[i]) != -1:
-            price.append(text[j][u"總價元"])
-    string = finalRoad[i].encode('utf-8') + ", 最高成交價: "
-    string += str(max(price)) + ", 最低成交價: " + str(min(price))
-    print string
-    string = ""
-
-    del price[0:len(price)]
+	for j in range(0, len(text)):
+		if text[j][argument].find(finalRoad[i]) != -1:
+			price.append(text[j][u"總價元"])
+			
+	string = finalRoad[i].encode('utf-8') + ", 最高成交價: "
+	string += str(max(price)) + ", 最低成交價: " + str(min(price))
+	print string
+	string = ""
+	
+	del price[0:len(price)]
